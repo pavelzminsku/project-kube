8b1247bfaddaf6dbb6e6c7dc7518b0f8d0155d62
igor.ulasevich <igor.ulasevich@patio-minsk.by>
2025-04-10 14:32:22 +0300

    fixed formated str

--- scripts/cluster.py
+++ scripts/cluster.py
@@ -17,11 +17,11 @@ def install_yc():
 
 def yc_login():
     user = os.getenv("USER")
-    profiles = subprocess.getoutput('/home/{user}/yandex-cloud/bin/yc config profile list')
+    profiles = subprocess.getoutput(f'/home/{user}/yandex-cloud/bin/yc config profile list')
     logging.debug(f"Profiles: {profiles}")
     if not profiles or not 'otus' in profiles:
         error = 0
-        error += os.system('/home/{user}/yandex-cloud/bin/yc config profile create otus')
+        error += os.system(f'/home/{user}/yandex-cloud/bin/yc config profile create otus')
         logging.debug(f"Creating profile errors: {error}")
         if not error:
             secret = os.getenv('SECRET')
@@ -29,21 +29,21 @@ def yc_login():
             with open('temp_key.json', 'w') as file:
                 file.write(secret)
             logging.debug(f"Adding file errors: {error}")
-            error += os.system('/home/{user}/yandex-cloud/bin/yc config set service-account-key temp_key.json')
+            error += os.system(f'/home/{user}/yandex-cloud/bin/yc config set service-account-key temp_key.json')
             logging.debug(f"Adding key errors: {error}")
             error += os.system('rm temp_key.json')
             logging.debug(f"Removing file errors: {error}")
             if not error:
-                error += os.system('/home/{user}/yandex-cloud/bin/yc config set cloud-id $CLOUD')
+                error += os.system(f'/home/{user}/yandex-cloud/bin/yc config set cloud-id $CLOUD')
                 logging.debug(f"Adding cloud errors: {error}")
-                error += os.system('/home/{user}/yandex-cloud/bin/yc config set folder-id $FOLDER')
+                error += os.system(f'/home/{user}/yandex-cloud/bin/yc config set folder-id $FOLDER')
                 logging.debug(f"Adding folder errors: {error}")
         if error:
             raise Exception 
         
 def create_cluster(cluster_name: str, nodes: int):
     user = os.getenv("USER")
-    clusters = json.loads(subprocess.getoutput('/home/{user}/yandex-cloud/bin/yc managed-kubernetes cluster list --format json'))
+    clusters = json.loads(subprocess.getoutput(f'/home/{user}/yandex-cloud/bin/yc managed-kubernetes cluster list --format json'))
     logging.debug(f'{clusters}')
     cluster_exists = 0
     if clusters:
@@ -62,7 +62,7 @@ def create_cluster(cluster_name: str, nodes: int):
                                     --zone ru-central1-a --cluster-ipv4-range "172.17.0.0/16" --service-ipv4-range \
                                     "172.18.0.0/16" --public-ip --version 1.31 --format json'))
         logging.debug(f"Creation cluster: \n {creation}")
-    nodes_exist = json.loads(subprocess.getoutput('/home/{user}/yandex-cloud/bin/yc managed-kubernetes node-group list --format json'))
+    nodes_exist = json.loads(subprocess.getoutput(f'/home/{user}/yandex-cloud/bin/yc managed-kubernetes node-group list --format json'))
     if not nodes_exist:
         logging.info(f'There is no nodes in cluster with name {cluster_name}, creating...')
         creation = json.loads(subprocess.getoutput(f'/home/{user}/yandex-cloud/bin/yc managed-kubernetes node-group create \
