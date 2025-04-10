0127429297358267eefd376f7778674cdf5e2dfc
igor.ulasevich <igor.ulasevich@patio-minsk.by>
2025-04-10 16:31:50 +0300

    fix values name

--- scripts/argocd.py
+++ scripts/argocd.py
@@ -9,7 +9,7 @@ logging.basicConfig(level="DEBUG")
 def install_argocd():
     installed = subprocess.getoutput('helm list -A -o json')
     os.system('helm repo add argo https://argoproj.github.io/argo-helm')
-    os.system('helm install argocd argo/argo-cd -n argocd -f ./values/argocd-values.yaml --create-namespace')
+    os.system('helm install argocd argo/argo-cd -n argocd -f values/argocd-values.yaml --create-namespace')
 
 
 def main():
