bc6e43766efadafd9343dd9c2c93ba4be30eb040
igor.ulasevich <igor.ulasevich@patio-minsk.by>
2025-04-14 08:44:27 +0300

    Final test on cleen YC

--- scripts/argocd.py
+++ scripts/argocd.py
@@ -4,25 +4,22 @@ import os
 import logging
 
 
-logging.basicConfig(level="DEBUG")
+logging.basicConfig(level="INFO")
 
-def install_argocd():
+def main():
     logging.debug(f"Installing argocd")
     installed = json.loads(subprocess.getoutput('/usr/local/bin/helm list -n argocd -o json'))
     logging.debug(f'Helm list: {installed}')
     if not installed:
         error = 0
-        logging.debug(f"No argocd found. Installing...")
+        logging.info(f"No argocd found. Installing...")
         error += os.system('/usr/local/bin/helm repo add argo https://argoproj.github.io/argo-helm')
         logging.debug(f'Adding repo, errors: {error}')
         error += os.system('/usr/local/bin/helm install argocd argo/argo-cd -n argocd -f values/argocd-values.yaml --create-namespace --wait')
         logging.debug(f'Installing argo, errors: {error}')
+    logging.info(f'Adding app of apps.')
     os.system('/usr/local/bin/kubectl apply -f app-of-apps.yaml')
-    
 
-def main():
-    install_argocd()
 
 if __name__ == "__main__":
     main()
-
